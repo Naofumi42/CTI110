600e1551ab89d29ee2f613067bcb08b13d51ca8d
kelly Brown <brownk5090@student.faytechcc.edu>
2024-09-21 23:54:17 -0400

    Add files via upload

---
+++ P1LAB1_BrownKelly.py
@@ -0,0 +1,12 @@
+ # Your Name (Kelly Brown)	
+ # Date (09/21/24)
+ # Assignment Name (P1LAB1)
+ # A brief description of the project
+
+
+first_name = input ('Enter your First name?')
+last_name = input ('Enter your Last name?')
+
+#name is an input which will be replaced by users name
+
+print ('Hello', first_name, last_name ,' Welcome to CTI-110')
